b5d88f19e0040c00e65935195be6bbdb9c3a2b08
Lisandro Ruiz <lisandroruiz05@gmail.com>
2026-04-16 00:04:08 -0300

    Menu Agregado

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -3,11 +3,13 @@ package app;
 import data.Persistencia;
 import java.util.InvalidPropertiesFormatException;
 import views.ListarVehiculosView;
+import views.Menu;
 
 public class Program {
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
-        ListarVehiculosView view = new ListarVehiculosView();
-        view.setVisible(true);
+        //ListarVehiculosView view = new ListarVehiculosView(this);
+           Menu v1 = new Menu();
+       v1.setVisible(true);
     }
 }
